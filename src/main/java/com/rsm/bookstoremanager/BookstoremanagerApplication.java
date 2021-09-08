c74d2688dd0ce64a02ee04511733d326e011db2d
Ricardo Murca <richard_sm1@outlook.com>
2021-09-08 02:30:37 -0300

    Projeto criado

---
+++ src/main/java/com/rsm/bookstoremanager/BookstoremanagerApplication.java
@@ -0,0 +1,12 @@
+package com.rsm.bookstoremanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoremanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoremanagerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/rsm/bookstoremanager/BookstoremanagerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.rsm.bookstoremanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoremanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
